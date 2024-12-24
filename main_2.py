e08f41d62204b567b5d14e737c81f0493cb18195
Varisht Raheja <varishtraheja@gmail.com>
2024-12-24 17:54:09 +0530

    Created better features

--- main_2.py
+++ main_2.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
